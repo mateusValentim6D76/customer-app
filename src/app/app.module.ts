fe7e1e46bba21649ea5ca8f8c91cc5a2d89da38b
mateusValentim6D76 <mateus.valentim19@hotmail.com>
2022-01-26 20:55:32 -0300

    Finalizando o template da home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { TemplateModule } from './template/template.module';
+import { HomeComponent } from './home/home.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
