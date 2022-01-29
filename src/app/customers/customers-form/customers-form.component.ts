2b53c5ded998338530cd17142e1aa2b98911bcb6
mateusValentim6D76 <mateus.valentim19@hotmail.com>
2022-01-28 21:32:00 -0300

    Corrigindo erro ao tentar carregar cliente por ID

--- src/app/customers/customers-form/customers-form.component.ts
+++ src/app/customers/customers-form/customers-form.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 //import { ActivatedRoute } from '@angular/router';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 import { Customer } from '../customer';
-import {CustomersService} from '../../customers.service'
+import { CustomersService } from '../../customers.service'
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-customers-form',
@@ -13,54 +14,56 @@ export class CustomersFormComponent implements OnInit {
   customer: Customer;
   success: boolean = false
   errors: String[];
-  id : number
+  id: number
 
   constructor(
-    private service : CustomersService,
-    private router : Router,
+    private service: CustomersService,
+    private router: Router,
     private activatedRoute: ActivatedRoute
-    ) { 
+  ) {
     this.customer = new Customer()
   }
 
   ngOnInit(): void {
-    let params = this.activatedRoute.params  
-    if(params && params._value && params._value.id){
-      this.id = params._value.id;
-      this.service
-      .getCustomerById(this.id)
-      .subscribe(
-        response => this.customer = response,
-        errorResponse => this.customer = new Customer()
-      )
-    }
+    let params: Observable<Params> = this.activatedRoute.params
+    params.subscribe(urlParams => {
+      this.id = urlParams['id'];
+      if (this.id) {
+        this.service
+          .getCustomerById(this.id)
+          .subscribe(
+            response => this.customer = response,
+            errorResponse => this.customer = new Customer()
+          )
+      }
+    })
   }
 
-  onSubmit(){
-    if(this.id){
+  onSubmit() {
+    if (this.id) {
       this.service
-      .update(this.customer)
-      .subscribe(response => {
-        this.success = true;
+        .update(this.customer)
+        .subscribe(response => {
+          this.success = true;
           this.errors = null;
-      }, errorResponse => {
-        this.errors = ['error updating client']
-      })
+        }, errorResponse => {
+          this.errors = ['error updating client']
+        })
     } else {
       this.service
-      .save(this.customer)
-      .subscribe(response => {
-        this.success = true;
-        this.errors = null;
-        this.customer = response
-      }, errorResponse => {
-        this.success = false;
-        this.errors = errorResponse.error.errors;
-      })
+        .save(this.customer)
+        .subscribe(response => {
+          this.success = true;
+          this.errors = null;
+          this.customer = response
+        }, errorResponse => {
+          this.success = false;
+          this.errors = errorResponse.error.errors;
+        })
     }
   }
 
-  returnListCustomers(){
+  returnListCustomers() {
     this.router.navigate(['/customers-list'])
   }
 
