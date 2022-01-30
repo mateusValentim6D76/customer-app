54b50a2d2f937c537d605525239c33837f8bcb6c
mateusValentim6D76 <mateus.valentim19@hotmail.com>
2022-01-30 18:27:51 -0300

    Criando formulário de pesquisas de Serviços Prestados

--- src/app/service-provided/service-provided-list/service-provided-list.component.ts
+++ src/app/service-provided/service-provided-list/service-provided-list.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./service-provided-list.component.css']
 })
 export class ServiceProvidedListComponent implements OnInit {
+  
+  name: string
+  month: number
+  months: number[]
 
-  constructor() { }
+  constructor() {
+    this.months = [1,2,3,4,5,6,7,8,9,10,11,12];
+  }
 
   ngOnInit(): void {
+  }
+
+  consult(){
+    console.log(this.name, this.month)
   }
 
 }
