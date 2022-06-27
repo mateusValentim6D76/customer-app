6f401d18815aed3b05e8ab8565d20e21ed002e15
mateusValentim6D76 <mateus.valentim19@hotmail.com>
2022-06-27 20:55:58 -0300

    Realizando o redirecionamento para tela de login caso nao esteja autenticado

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 
@@ -10,13 +10,20 @@ import { AuthService } from './auth.service';
 export class AuthGuard implements CanActivate {
 
   constructor(
-    private authService: AuthService
+    private authService: AuthService,
+    private router : Router
   ){}
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): boolean {
-    return this.authService.isAuthenticated()
+    const authenticated =  this.authService.isAuthenticated()
+
+    if(authenticated){
+      return true;
+    }else{
+      this.router.navigate(['/login'])
+    }
   }
   
 }
