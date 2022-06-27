84c3e3e5590c1cc110e02bc2d2a9cecd7194365b
mateusValentim6D76 <mateus.valentim19@hotmail.com>
2022-06-27 20:48:44 -0300

    adicionando pacote angular jwt e implementando logica de controlar a sessao do user com o token na tela home

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { User } from './login/user';
 import { environment } from '../environments/environment';
 import { Observable } from 'rxjs';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
@@ -13,13 +14,28 @@ export class AuthService {
   tokenURL : string = environment.apiUrlBase + environment.getTokenUrl;
   clientId: string = environment.clientId;
   clientSecret: string = environment.clientSecret;
+  jwtHelper : JwtHelperService = new JwtHelperService();
 
   constructor( private http: HttpClient ) {  }
 
+obterToken(){
+  const tokenString = localStorage.getItem('access_token')
+  if(tokenString){
+    const token = JSON.parse(tokenString).access_token
+    return token;
+  }
+  return null;
+}
+
   isAuthenticated() : boolean{
+    const token = this.obterToken();
+    if (token){
+      const expired = this.jwtHelper.isTokenExpired(token)
+      return !expired;
+    }
     return false;
   }
-
+  
  saveUser(user: User) : Observable<any>{
   return this.http.post<any>(this.apiURL, user)
  }
